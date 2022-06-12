c4029683e8d75c8ae6fae8daac6fda8177336e69
Alberto <94637799+meaar@users.noreply.github.com>
2022-06-11 22:00:04 -0300

    update de melhorias no algoritmo

--- crivo-sequencial.c
+++ crivo-sequencial.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define NUMBER 50
+#define NUMBER 1000
 
 int main(){
     int i,j;
-    int max = sqrt(NUMBER);
+    int limit = sqrt(NUMBER);
     int *vet = malloc(NUMBER*sizeof(int));
     for (i=0; i < NUMBER-1; i++){
         vet[i] = i+2;
     }
-    for (i=2; i <= max; i++){
+    for (i=2; i <= limit; i++){
         for (j=i+1; j <= NUMBER; j++){
-            if (j%i == 0){
+            if (vet[j-2]%i == 0){
                 vet[j-2] = -1;
             }
         }
     }
     for (i=0; i < NUMBER-1; i++){
-        printf("[%d]\n",vet[i]);
+        if (vet[i] != -1){
+            printf("[%d]\n",vet[i]);
+        }
     }
     return 0;
 }
